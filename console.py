b133a2b1ea9f076c97b0247522a2cf8a63b05ee5
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-03-05 19:28:17 +0200

    (Task 6) Updating console.py file

--- console.py
+++ console.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
 
 import cmd
-import os
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-
 
     def do_EOF(self, line):
         """Recieves the end of a file marker and returns True"""
         print()
         return True
-    
-    def precmd(self, line: str):
-        if not os.isatty(0) and line.strip():
-            print()
-        return super().precmd(line)
 
     def do_quit(self, line):
-        """Exits the current process"""
+        """Quit command to exit the program"""
         return True
-    
-    def do_test(self, line):
-        """ test """
-        print("hi")
-    
+
     def emptyline(self):
         """Returns an empty string if no command is entered"""
         pass
 
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
-
-
