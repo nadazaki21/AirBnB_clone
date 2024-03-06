cc17b89786d155e5ae1accb48abde2b0a78e48f8
nadazkai21 <nadazaki019@gmail.com>
2024-03-06 18:36:46 +0200

    tsk 0 finished

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,6 +49,11 @@ class FileStorage:
         """
         from models.base_model import BaseModel
         from models.user import User
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.amenity import Amenity
+        from models.state import State
 
         try:
             with open(FileStorage.__file_path, "r") as file:
